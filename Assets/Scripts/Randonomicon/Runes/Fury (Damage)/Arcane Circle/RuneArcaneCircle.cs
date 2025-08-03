c1dee9d11a951350b15be905a0b22df8099f68f2
Jose Daniel Cabrera Calderón <jdev.cabrerac@gmail.com>
2025-08-02 22:45:53 -0500

    fixed arcane circle

--- Assets/Scripts/Randonomicon/Runes/Fury (Damage)/Arcane Circle/RuneArcaneCircle.cs
+++ Assets/Scripts/Randonomicon/Runes/Fury (Damage)/Arcane Circle/RuneArcaneCircle.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class RuneArcaneCircle : PassiveRuneStateBase
 {
     private GameObject runeInstance;
+    private ArcaneCircleDamage arcaneCircleComponent;
 
     public RuneArcaneCircle(RuneStateData runeStateData) : base(runeStateData)
     {
@@ -13,15 +14,35 @@ public class RuneArcaneCircle : PassiveRuneStateBase
         base.Enter(owner);
         runeInstance = GameObject.Instantiate(_stateData.GetPrefab("circle01"), owner.transform);
         runeInstance.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
-        ArcaneCircleDamage arcaneCircle = runeInstance.GetComponent<ArcaneCircleDamage>();
-        arcaneCircle.owner = owner;
-        arcaneCircle.damage = StatsComponent.Get(owner).currentStats.damage;
-        arcaneCircle.criticalChance = StatsComponent.Get(owner).currentStats.criticalChance;
+        arcaneCircleComponent = runeInstance.GetComponent<ArcaneCircleDamage>();
+        arcaneCircleComponent.owner = owner;
+        // Set initial values
+        UpdateDamageStats();
+    }
+
+    public override void Tick(float dt)
+    {
+        base.Tick(dt);
+        // Update damage stats every frame to reflect current player stats
+        if (arcaneCircleComponent != null)
+        {
+            UpdateDamageStats();
+        }
+    }
+
+    private void UpdateDamageStats()
+    {
+        var stats = StatsComponent.Get(owner);
+        arcaneCircleComponent.damage = stats.currentStats.damage;
+        arcaneCircleComponent.criticalChance = stats.currentStats.criticalChance;
     }
 
     public override void Exit()
     {
         base.Exit();
-        GameObject.Destroy(runeInstance);
+        if (runeInstance != null)
+        {
+            GameObject.Destroy(runeInstance);
+        }
     }
 }
